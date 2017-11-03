13a5ef4ba08a990dc81c60afdecd0f5761badaa1
mateyo3 <mateoreyes720@gmail.com>
2017-11-02 17:54:50 -0700

    readFile words.txt

--- game.js
+++ game.js
@@ -1,40 +1,56 @@
 var inquirer = require("inquirer");
 
-//start questions
-inquirer.prompt([
-//input user name
-{
-    type: "input",
-    name: "name",
-    message: "Who are you?"
-  },
+var fs = require("fs");
+fs.readFile("words.txt", "utf8", function(error, data) {
 
-  {
-    type: "confirm",
-    name: "play",
-    message: "Do you want to play Hangman?",
-    default: true
-  },
+	if (error) {
+	return console.log(error);
+	}
 
-]).then(function(user) {
+	console.log("Every word in the array:\n" + data);
 
-  if (user.play === true) {
-    console.log("Let's begin!");
-    game();
-  }
- });
+	// var wordArr = data.toString().split("/n");
+	// var guessWord = wordArr(Math.floor(Math.random() * wordArr.length));
+	// console.log("The word is: " + guessWord);
+});
 
-var letterGuessed =[];
+// //start questions
+// inquirer.prompt([
+// //input user name
+// {
+//     type: "input",
+//     name: "name",
+//     message: "Who are you?"
+//   },
+
+//   {
+//     type: "confirm",
+//     name: "play",
+//     message: "Do you want to play Hangman?",
+//     default: true
+//   },
+
+// ]).then(function(user) {
+
+//   if (user.play === true) {
+//     console.log("Let's begin!");
+//     game();
+//   }
+//  });
+
+// var letterGuessed =[];
 
 
-function game(){
-	console.log("Directions: Press a key to guess the letters in the word.")
-}
+// function game(){
+// 	console.log("Directions: Press a key to guess the letters in the word.")
+// }
 
-function Word(){
+// function Word(){
 
-}
+// }
 
-function Letter(){
+// function Letter(){
 
-}
+// }
+
+
