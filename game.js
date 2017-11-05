4a4addccad6b0958363b945c1747b123d4053363
mateyo3 <mateoreyes720@gmail.com>
2017-11-05 08:03:00 -0800

    syntax for randomWord function

--- game.js
+++ game.js
@@ -1,18 +1,43 @@
-var inquirer = require("inquirer");
+//Global variables & arrays
+var guessWord = "";
+var blanks = 0;
+var guessesRemaining = 9;
+var guessWordLetters = [];
+var letterGuessed =[];
+var blanksAndCorrect = [];
 
-var fs = require("fs");
-fs.readFile("words.txt", "utf8", function(error, data) {
 
-	if (error) {
-	return console.log(error);
-	}
+//Read text from words.txt file to choose random word
+function randomWord() {
+	var fs = require("fs");
+	fs.readFile("words.txt", "utf8", function(error, data) {
 
-	console.log("Every word in the array:\n" + data);
+		if (error) {
+		return console.log(error);
+		}
 
-	// var wordArr = data.toString().split("/n");
-	// var guessWord = wordArr(Math.floor(Math.random() * wordArr.length));
-	// console.log("The word is: " + guessWord);
-});
+		//check that the words.txt file is being read
+		console.log("Every word in the words.txt\n******\n" + data);
+
+		var wordArr = data.toString().split("\n");
+		guessWord = wordArr[Math.floor(Math.random() * wordArr.length)];
+		
+		//check that random word is working
+		console.log("******");
+		console.log("The word is: " + guessWord);
+	});
+}//end: function(word)
+
+//run randomWord function
+randomWord();
+
+
+//split guessWord into array of letters
+var letterArray =[];
+
+
+// //start inquirer for user input
+// var inquirer = require("inquirer");
 
 // //start questions
 // inquirer.prompt([
@@ -38,19 +63,38 @@ fs.readFile("words.txt", "utf8", function(error, data) {
 //   }
 //  });
 
-// var letterGuessed =[];
+
 
 
 // function game(){
 // 	console.log("Directions: Press a key to guess the letters in the word.")
 // }
 
-// function Word(){
+// //constructor function: Word
+// function Word(word){
+// 	this.word = word;
+// }// end: constructor function: Word
 
-// }
 
+// //constructor function: Letter
 // function Letter(){
+// 	this.letter = letter;
+// }//end: constructor function: Letter
+
+// var askQuestion = function() {
+
+// 	if (count < 3) {
+// 		inquirer.prompt([
+// 			{
+// 				name: "question"
+// 				message: "What's your guess?"
+// 			}
+// 		])
+// 	}
+
+// 	}.then(function(checkLetter){
+
+// 		askQuestion();
+// 		});
 
 // }
-
-
