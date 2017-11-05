437e05ec11cb51012b2139a0e97c7ef885be2e85
mateyo3 <mateoreyes720@gmail.com>
2017-11-05 08:32:04 -0800

    replaced letters with blanks

--- game.js
+++ game.js
@@ -5,9 +5,10 @@ var guessesRemaining = 9;
 var guessWordLetters = [];
 var letterGuessed =[];
 var blanksAndCorrect = [];
+var incorrectGuess = [];
 
 
-//Read text from words.txt file to choose random word
+//Read text from words.txt file to choose random word and display blanks
 function randomWord() {
 	var fs = require("fs");
 	fs.readFile("words.txt", "utf8", function(error, data) {
@@ -28,10 +29,22 @@ function randomWord() {
 
 		//split guessWord into array of letters
 		guessWordLetters = guessWord.split("");
+		//check that .split is working
 		console.log("This is an array: " + guessWordLetters);
 
 		//displays blanks based on number of letters in guessWordLetters array
-		blanks = 
+		blanks = guessWordLetters.length;
+		console.log("Number of letters in guessWord: " + blanks);
+
+		blanksAndCorrect = [];
+		incorrectGuess = [];
+
+		for (var i = 0; i < guessWordLetters.length; i++){
+			blanksAndCorrect.push("_");
+		}
+
+		//display letters in console as blanks
+		console.log("blanksAndCorrect: " + blanksAndCorrect);
 	});
 
 
